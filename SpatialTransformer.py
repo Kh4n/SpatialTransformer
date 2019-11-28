d0ebdb21ddb0378c23be2649abe75ee222a6c49e
Kh4n <asadullahmkhan@gmail.com>
2019-11-28 16:52:36 -0600

    i have found the problem that plagued me earlier. details in diff (added comments, will clean up main sometime)

--- SpatialTransformer.py
+++ SpatialTransformer.py
@@ -6,10 +6,21 @@ class SpatialTransformer(tf.keras.layers.Layer):
         super(SpatialTransformer, self).__init__(**kwargs)
 
     def build(self, input_shape):
+        # grab the dimensions of the image here so we can use them later. also will throw errors early for users
         self.h = input_shape[1][1]
         self.w = input_shape[1][2]
         self.c = input_shape[1][3]
-        x_t, y_t = tf.meshgrid(tf.linspace(-1.0, 1.0, self.w), tf.linspace(-1.0, 1.0, self.h))
+
+        # need to get the axis that has the max length so we can scale relatively to that
+        self.max_hw = max(self.h, self.w)
+
+        # this where many other implementations do not work correctly. you must create the meshgrid with sizes relative to the max dimension.
+        # this is what it used to look like for reference: x_t, y_t = tf.meshgrid(tf.linspace(-1.0, 1.0, self.w), tf.linspace(-1.0, 1.0, self.h))
+        # essentially, one dimension gets skewed because it has less samples (last arg of tf.linspace) but is still scaled to [-1,1]
+        x_t, y_t = tf.meshgrid(
+            tf.linspace(-self.w/self.max_hw, self.w/self.max_hw, self.w),
+            tf.linspace(-self.h/self.max_hw, self.h/self.max_hw, self.h)
+        )
         self.sampling_grid = tf.stack([tf.reshape(x_t, [self.h*self.w]), tf.reshape(y_t, [self.h*self.w]), tf.ones(self.h*self.w, tf.float32)])
 
         super(SpatialTransformer, self).build(input_shape)
@@ -18,11 +29,13 @@ class SpatialTransformer(tf.keras.layers.Layer):
         local = inputs[0]
         imgs = inputs[1]
 
+        # -1 as reshape automatically infers batch dimension
         transforms = tf.reshape(local, [-1, 2, 3])
         samples = tf.matmul(transforms, self.sampling_grid)
 
-        x = ((samples[:, 0] + 1) * self.w) * 0.5
-        y = ((samples[:, 1] + 1) * self.h) * 0.5
+        # have to adjust to the relative scaling done earlier
+        x = ((samples[:, 0] + self.w/self.max_hw) * self.max_hw) * 0.5
+        y = ((samples[:, 1] + self.h/self.max_hw) * self.max_hw) * 0.5
 
         x0 = tf.floor(x)
         x1 = x0 + 1
@@ -44,7 +57,6 @@ class SpatialTransformer(tf.keras.layers.Layer):
         y0 = tf.cast(y0, tf.int32)
         y1 = tf.cast(y1, tf.int32)
 
-        y0_x0 = tf.stack([y0, x0], axis=-1)
         Ia = tf.gather_nd(imgs, tf.stack([y0, x0], axis=-1), batch_dims=1)
         Ib = tf.gather_nd(imgs, tf.stack([y0, x1], axis=-1), batch_dims=1)
         Ic = tf.gather_nd(imgs, tf.stack([y1, x0], axis=-1), batch_dims=1)
@@ -74,8 +86,16 @@ if __name__ == "__main__":
           math.sin(math.pi/6),  math.cos(math.pi/6), 0.]]*batches,
         dtype=np.float32
     )
+
+    # identity transform, for testing purposes
+    # transforms = np.asarray(
+    #     [[1., 0., 0.,
+    #       0., 1., 0.]]*batches,
+    #     dtype=np.float32
+    # )
     imgs = np.asarray([cv.imread("random.jpg"), cv.imread("random.jpg")]).astype(np.float32)
+    imgs = np.asarray([cv.imread("square.jpg"), cv.imread("square.jpg")]).astype(np.float32)
     out = SpatialTransformer()([transforms, imgs])
     print(np.shape(out))
-    cv.imshow("out", np.float32(out[0]) / 255)
+    cv.imshow("the image should be rotated 30deg ccw", np.float32(out[0]) / 255)
     cv.waitKey()
